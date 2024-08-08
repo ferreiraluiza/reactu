c4995a6883c7cc655b23fd6467486975aecb1dff
luluferreira13 <ra24204696821@acaonsfatima.org.br>
2024-08-08 09:34:05 -0300

    first commit

---
+++ discord/src/pages/app/index.jsx
@@ -0,0 +1,23 @@
+export default function App() {
+    return (
+        <div className="primeira-div">
+            <img src="/tanjerina.jpg" alt="" />
+            
+            <div className="segunda-div">
+                <div className="textin-1">
+                    <h1>Ronekos Star ★</h1>
+                </div>
+
+                <div className="textin-2">
+                    <p>Aqui no Ronekos Star Você pode Receber Ajuda no Roblox e Temos Categoria de Jogos Também Você será Muito Bem Vindo(a) ^^
+                    </p>
+                </div>
+
+                <div className="textin-flex">
+                    <p>525 online</p>
+                    <p>3.647 Members </p>
+                </div>
+            </div>
+        </div>
+    )
+}
